4be2e0fa69aff04dbae7d64154c25247218d203b
9ani <Gankon17@gmail.com>
2024-06-13 11:11:22 +0500

    update

--- server/nf-back-l2-back/src/db.ts
+++ server/nf-back-l2-back/src/db.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/chatApp');
+        await mongoose.connect("mongodb+srv://gani:qwerty123456@weatherapi.qd1uz2q.mongodb.net/backend-homework-1?retryWrites=true&w=majority");
         console.log('MongoDB connected...');
     } catch (err: any) {
         console.error(err.message);
